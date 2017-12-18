8280d4a786074c2badd611fa5a36cdb8b9d4bbef
mika <unitycoder@users.noreply.github.com>
2017-12-18 16:37:48 +0800

    Update terrain-example.lua

--- Terrain/terrain-example.lua
+++ Terrain/terrain-example.lua
@@ -5,9 +5,12 @@ b:ClearObjects()
 id="terrain"
 size=100 -- in meters
 px=-size/2
-py=-10
+py=-20
 pz=px
 b:CreateTerrain(id,size,px,py,pz)
+
+-- perlinnoise heightmap
+b:SetTerrainHeightmapFromPerlinNoise(id,0.007,0.05)
 
 -- hide photospheres, if in that mode
 b:PhotoSphere_HideSpheres()
@@ -16,6 +19,16 @@ b:PhotoSphere_HideSpheres()
 b:TogglePlayerMovement(true)
 b:SetPlayerMovementSpeed(15)
 b:AddTerrainEdgeColliders(id)
+
+x=0
+z=0
+y = b:GetTerrainWorldHeight(id,x,z)
+--b:Log(y)
+
+-- align cube on terrain surface
+cube = "cube"
+b:CreateCube(cube,1,1,1,x,y,z)
+b:DropAndAlignObjectToTerrain(cube,id,0,0,0)
 
 -- mainloop TODO
 return function()
