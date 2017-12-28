48475db083c55ece5f3da80db47dab4b09b3f69a
mika <unitycoder@users.noreply.github.com>
2017-12-28 14:00:54 +0800

    Update get-position.lua

--- Queries/get-position.lua
+++ Queries/get-position.lua
@@ -2,6 +2,7 @@ b = BinooAPI
 b:CreateCube("box",1,1,1,3,0,0)
 -- returns float[] array x,y,z
 p = b:GetPosition("box")
-b:Log(p[0]) --x
-b:Log(p[1]) --y
-b:Log(p[2]) --z
+-- NOTE: LUA array starts from 1
+b:Log(p[1]) --x
+b:Log(p[2]) --y
+b:Log(p[3]) --z
