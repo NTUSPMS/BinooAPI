d705e1700327592eb6fa5025266d273f272c9388
mika <unitycoder@users.noreply.github.com>
2017-12-28 12:43:15 +0800

    Update get-position.lua

--- Queries/get-position.lua
+++ Queries/get-position.lua
@@ -1,4 +1,7 @@
 b = BinooAPI
 b:CreateCube("box",1,1,1,3,0,0)
--- return vector3 as a comma separated string x,y,z
-b:Log(b:GetPosition("box"))
+-- returns float[] array x,y,z
+p = b:GetPosition("box")
+b:Log(p[0]) --x
+b:Log(p[1]) --y
+b:Log(p[2]) --z
